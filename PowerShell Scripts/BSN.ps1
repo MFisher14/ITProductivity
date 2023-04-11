e53f385b1debbdf44cddcdaba228df036ad51518
Maximus <fish4max@gmail.com>
2023-04-11 08:42:24 -0400

    Create BSN.ps1

---
+++ PowerShell Scripts/BSN.ps1
@@ -0,0 +1,9 @@
+﻿Connect-AzureAD
+New-AzureADGroup -DisplayName 'BSN-Employees' -MailEnabled $false -MailNickName 'BSN-Employees' -SecurityEnabled $true
+New-AzureADGroup -DisplayName 'BSN-Managers' -MailEnabled $false -MailNickName 'BSN-Managers' -SecurityEnabled $true
+
+
+$AzureUsers = Get-AzureADUser
+$NumberOfUsers = $AzureUsers.count + 1
+$CountingVariable = 0
+while ($CountingVariable -ne $NumberOfUsers) {
