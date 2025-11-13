6a23b8330406ca35546bb5bcf1994fce4c3ff7a8
Maximus <fish4max@gmail.com>
2025-11-13 14:26:09 -0500

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,3 +1,13 @@
 Write-Host "Hello World"
 ipconfig
 Get-LocalUser
+
+
+Add-Type -AssemblyName System.Windows.Forms
+
+# Customize the message and title
+$message = "Please call the IT Helpdesk at 555-1234 for assistance."
+$title = "Helpdesk Notification"
+
+# Show the message box
+[System.Windows.Forms.MessageBox]::Show($message, $title, 'OK', 'Information')
